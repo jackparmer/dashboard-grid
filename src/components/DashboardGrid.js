4bd691db122c6278806bd54be0381faa4634c893
JP <jparmer09@gmail.com>
2017-05-18 18:28:28 -0400

    add tables

--- src/components/DashboardGrid.js
+++ src/components/DashboardGrid.js
@@ -10,22 +10,30 @@ import Plotly from 'plotly.js';
 import createPlotlyComponent from '../components/plotlyjs.react.js';
 const PlotlyComponent = createPlotlyComponent(Plotly);
 
+import ReactTable from 'react-table'
+
 import '../css/normalize.css';
 import '../css/skeleton.css';
 import '../css/styles.css';
+import '../css/react-table.css';
+
+const mapUrl = 'https://raw.githubusercontent.com/jackparmer/dashboard-grid/master/test_data/eb_ob_ggplot2_restyled.json'
+const obDeathsUrl = 'https://raw.githubusercontent.com/jackparmer/dashboard-grid/master/test_data/eb_ob_barchart.json'
+const obDeathsCumulativeUrl = 'https://raw.githubusercontent.com/jackparmer/dashboard-grid/master/test_data/eb_ob_line.json'
+const obTable = 'https://raw.githubusercontent.com/jackparmer/dashboard-grid/master/test_data/eb_ob.csv'
 
 class PlotForm extends React.Component {
     constructor(props) {
         super(props);
         this.state = {value: ''};
 
-        this.handleChange = this.handleChange.bind(this);
+        this.handleFormInputChange = this.handleFormInputChange.bind(this);
 
         this.handleFormSubmit = this.handleFormSubmit.bind(this);
         this.handleFormClose = this.handleFormClose.bind(this);
     }
 
-    handleChange(event) {
+    handleFormInputChange(event) {
         this.setState({value: event.target.value});
     }
 
@@ -34,17 +42,33 @@ class PlotForm extends React.Component {
 
         var fig;
         var parent = this;
+        var urlToFetch = this.state.value;
 
-        Plotly.d3.json( this.state.value, 
-            function(error, figure) {
-
-                console.log( error, figure );              
-                fig = figure;
-                console.log('FIG LAYOUT', fig.layout);
-                parent.props.handleSubmit( { data:fig.data, layout:fig.layout } );
-        } );
-        
-        //this.props.handleSubmit( { data:fig.data, layout:fig.layout } );
+        if( urlToFetch.substr(urlToFetch.length -3).toLowerCase() != 'csv'  ){
+            Plotly.d3.json( urlToFetch, 
+                function(error, figure) {
+                    if( error ){
+                        console.log( error, figure );              
+                        // maybe file is  a CSV instead
+                        Plotly.d3.csv( urlToFetch,
+                            function(error, csvArray ) {
+                                parent.props.handleSubmit( csvArray );
+                            });
+                    }
+                    else{
+                        fig = figure;
+                        console.log('FIG LAYOUT', fig.layout);
+                        parent.props.handleSubmit( { data:fig.data, layout:fig.layout } );
+                    }
+                } );
+        }
+        else{
+            Plotly.d3.csv( urlToFetch,
+                function(error, csvArray ) {
+                    parent.props.handleSubmit( csvArray );
+                           });                
+        }
+            
     }
 
     handleFormClose(event){
@@ -56,36 +80,49 @@ class PlotForm extends React.Component {
             <form onSubmit={this.handleFormSubmit} style={styles.plotForm}>
                 <label>Link to raw Plotly graph JSON:</label>
                 <input type="text" value={this.state.value} 
-                        onChange={this.handleChange} style={styles.plotLinkInput} />
+                        onChange={this.handleFormInputChange} style={styles.plotLinkInput} />
                 <input type="submit" value="Submit" className="button-primary" />
                 <button onClick={this.handleFormClose} style={styles.cornerIcon} >Close</button>
                 <p>Examples (copy / paste):</p>
-                <p><a href="#">https://raw.githubusercontent.com/plotly/plotly.js/master/test/image/mocks/airfoil.json</a></p>
-                <p><a href="#">https://raw.githubusercontent.com/plotly/plotly.js/master/test/image/mocks/sankey_energy.json</a></p>
-                <p><a href="#">https://raw.githubusercontent.com/plotly/plotly.js/master/test/image/mocks/sankey_energy_dark.json</a></p>
-                <p><a href="#">https://raw.githubusercontent.com/plotly/plotly.js/master/test/image/mocks/geo_usa-states.json</a></p>  
-                <p><a href="#">https://raw.githubusercontent.com/plotly/plotly.js/master/test/image/mocks/candlestick_rangeslider_thai.json</a></p>
+                <p>Map:<br/><a href="#">{ mapUrl }</a></p>
+                <p>Outbreak Deaths:<br/><a href="#">
+                    https://raw.githubusercontent.com/jackparmer/dashboard-grid/master/test_data/eb_ob_barchart.json
+                </a></p>
+                <p>Outbreak Deaths (cumulative):<br/><a href="#">
+                    https://raw.githubusercontent.com/jackparmer/dashboard-grid/master/test_data/eb_ob_line.json
+                </a></p>
+                <p>CSV Table:<br/><a href="#">
+                    https://raw.githubusercontent.com/jackparmer/dashboard-grid/master/test_data/eb_ob.csv
+                </a></p>
             </form>
         );
     }
 }
 
-function dfltPlot(){
-	let data = [ {
-	    type: 'scatter',  
-	    x: [1, 2, 3],     // more about "x": #scatter-x 
-	    y: [6, 2, 3],     // #scatter-y 
-	    marker: { color: 'navy'  }
-	}, {
-	    type: 'bar',      // all "bar" chart attributes: #bar 
-	    x: [1, 2, 3],     // more about "x": #bar-x 
-	    y: [6, 2, 3],     // #bar-y 
-	    name: 'bar chart example' // #bar-name 
-	} ];
+function dfltPlot( plotChoice ){
 
-	let lyo = { margins: {t:40, b:40, l:40, r: 40}, autosize: true, showlegend: false };        
+    var url;
 
-	return { data:data, layout:lyo }
+    switch( plotChoice ){
+        case 1:
+            url = mapUrl; break;
+        case 2: 
+            url = obDeathsUrl; break;
+        case 3:
+            url = obDeathsCumulativeUrl; break;
+        default:
+            url = mapUrl; break;
+    }
+
+    console.log('Retrieving URL', url);
+
+    Plotly.d3.json( url,
+        function(error, figure) {          
+            if( error ){ console.log( error ) }
+            console.log(figure);
+            return figure;
+        }
+    );
 };
 
 
@@ -93,6 +130,7 @@ class DashboardGrid extends React.Component{
 
 	constructor(props) {
 		super(props);
+        
 	};
 
 	static defaultProps = {
@@ -108,14 +146,15 @@ class DashboardGrid extends React.Component{
 		mounted: false,
 		layouts: {lg: this.props.initialLayout},
         items: [
-			{ x: 0, y:0, w: 4, h:10, i: '0', data: dfltPlot().data, layout: dfltPlot().layout  },
-			{ x: 4, y:0, w: 4, h:10, i: '1', data: dfltPlot().data, layout: dfltPlot().layout  },      
-			{ x: 8, y:0, w: 4, h:10, i: '2', data: dfltPlot().data, layout: dfltPlot().layout  },      
+			{ x: 0, y:0, w: 4, h:10, i: '0', figure: dfltPlot() },
+			{ x: 4, y:0, w: 4, h:10, i: '1', figure: dfltPlot() },      
+			{ x: 8, y:0, w: 4, h:10, i: '2', figure: dfltPlot() },      
 		],
 		newCounter: 0,
         showPlotForm: false,
         plotInEditMode: false,
-        newPlot: { data: [], layout: {} }
+        newPlot: { data: [], layout: {} },
+        newTable: []
 	};
 
 	componentDidMount = () => {
@@ -146,23 +185,53 @@ class DashboardGrid extends React.Component{
                        mapboxAccessToken: 'pk.eyJ1IjoiamFja3AiLCJhIjoidGpzN0lXVSJ9.7YK6eRwUNFwd3ODZff6JvA' };
 
 		let i = el.i;
+        let drawPlot = true;
 
         if( this.state.plotInEditMode !== false ){
-            if( this.state.plotInEditMode === i && this.state.newPlot.data.length !== 0 ){
-                console.log('writing new plot', this.state);
-                el.data = this.state.newPlot.data;
-                el.plotly_layout = this.state.newPlot.layout;
+            if( this.state.plotInEditMode === i ){
+                if( this.state.newPlot.data.length !== 0 ){
+                    console.log('writing new plot', this.state);
+                    el.data = this.state.newPlot.data;
+                    el.plotly_layout = this.state.newPlot.layout;
+                }
+                else if( this.state.newTable.length !== 0 ){
+                    drawPlot = false;
+                    el.dataTable = this.state.newTable;
+                    let columnHeaders = Object.keys( this.state.newTable[0] );
+                    el.dataColumns = columnHeaders.map( function(col){ return { Header: col, accessor: col } } )
+                }
             }
+
         }
- 
-		return (
-		    <div key={i} data-grid={el} >
+        if( drawPlot ){
+   		    return (
+  		        <div key={i} data-grid={el} >
                 	<span style={[styles.cornerIcon, styles.iconLeft]} onClick={this.editPlot.bind(this, i)}>Edit </span>
                 	<span style={[styles.cornerIcon, styles.iconRight]} onClick={this.onRemoveItem.bind(this, i)}> x</span>
-                	<PlotlyComponent className="plotly-container" id={"plot"+i.toString()} style={styles.plotDivStyle}
-		                data={el.data} layout={el.layout} config={config}/>
-		    </div>		                        
+                	<PlotlyComponent 
+                        className="plotly-container" 
+                        id={"plot"+i.toString()} 
+                        style={styles.plotDivStyle}
+                        onSelected={this.crossfilter}
+		                data={el.data} 
+                        layout={el.layout} 
+                        config={config}/>
+		        </div>		                        
         	)
+        }else{
+            return (
+                <div key={i} data-grid={el} >
+                    <span style={[styles.cornerIcon, styles.iconLeft]} onClick={this.editPlot.bind(this, i)}>Edit </span>
+                    <span style={[styles.cornerIcon, styles.iconRight]} onClick={this.onRemoveItem.bind(this, i)}> x</span>
+                    <ReactTable
+                        data={el.dataTable}
+                        columns={el.dataColumns}
+                        showPagination={false}
+                        showPageSizeOptions={false}
+                    />
+                </div>
+            )            
+        }
 	};
 
     onAddItem = () => {
@@ -176,8 +245,7 @@ class DashboardGrid extends React.Component{
 			    y: Infinity, // puts it at the bottom
 			    w: 4,
 			    h: 10,
-                data: dfltPlot().data, 
-                layout: dfltPlot().layout
+                figure: dfltPlot(), 
 		    }),
 	    	// Increment the counter to ensure key is always unique.
 	    	newCounter: this.state.newCounter + 1
@@ -189,15 +257,25 @@ class DashboardGrid extends React.Component{
         this.setState( { plotInEditMode: i.toString(), showPlotForm: true } );
     }
 
-    handleNewPlot = ( newPlotObj ) => {
-        console.log( 'plotting new plot', newPlotObj );
-        this.setState({ newPlot: newPlotObj, showPlotForm: false });
+    crossfilter = ( e ) => {
+        console.log( 'selection made', e );
+    }
+
+    handleNewPlot = ( newPlotOrTableObj ) => {
+        console.log( 'plotting new plot', newPlotOrTableObj );
+        if( newPlotOrTableObj.hasOwnProperty('layout') ){  
+            this.setState({ newPlot: newPlotOrTableObj, showPlotForm: false });
+        }
+        else{
+            this.setState({ newTable: newPlotOrTableObj, showPlotForm: false });
+        }
     }
 
     closePlotForm = () => {
         console.log('closing plot form');
         this.setState({
                     newPlot: { data: [], layout: {} },
+                    newTable: [],
                     plotInEditMode: false, 
                     showPlotForm: false });
     }   
@@ -244,7 +322,7 @@ var styles = {
     plotDivStyle: { height: '100%', width: '100%' },
     editPanel: { position: 'absolute', bottom: '10px', left: '10px', zIndex: 99 },
     plotForm: { padding: '50px 100px', border: '1px solid #ccc', maxWidth: '700px', 
-                width: '50%', margin: '0 auto', borderRadius: '2px', maxHeight: '400px', 
+                width: '50%', margin: '0 auto', borderRadius: '2px', maxHeight: '450px', 
                 position: 'absolute', left: 0, right: 0, top: '200px', bottom: 0, 
                 background: 'whitesmoke', height: '50%' },
     plotLinkInput: { width: '100%', padding: '10px', borderRadius: '4px', 
